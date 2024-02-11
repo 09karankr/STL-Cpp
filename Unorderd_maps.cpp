90f472404a76c8c4116e176e59abfeee3ad6fe51
Karan Kumar <officialkaran39@gmail.com>
2024-02-11 17:44:21 +0530

    done unordered maps

--- Unorderd_maps.cpp
+++ Unorderd_maps.cpp
@@ -8,5 +8,31 @@ using namespace std;
  // 2. time complexity - in unorderd maps TC is O(1) for insertion and also accessing 
  
 int main(){
+     //unordered_map<pair<int,int>,string> m;    unordered map does not support pair
+
+// now lets see a que on unordered_map
+/*
+Given N strings and Q queries.In each query you are given a string print frequency of that string
+ N<= 10^6
+ |S| <= 100
+ Q <= 10^6
+*/
+
+unordered_map<string,int> m;
+int n;
+cin>>n;
+for (int i = 0; i < n; i++)
+{
+     string s;
+     cin>>s;
+     m[s]++;
+}
+int q;
+cin>>q;
+while(q--){
+     string s;
+     cin>> s;
+     cout<<m[s]<<endl;
+}
 
 }
