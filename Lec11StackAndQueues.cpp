f70c16acb14efdbf1fd23c63e809bfa3a99c9421
Karan Kumar <officialkaran39@gmail.com>
2024-04-17 15:33:05 +0530

    completed lec 11 and done balanced brackets matching que

--- Lec11StackAndQueues.cpp
+++ Lec11StackAndQueues.cpp
@@ -26,4 +26,26 @@ int main(){
         q.pop();
     }
     
+    // que Balanced brackets hackerank
+
+    unordered_map<char,int> symbols = {{'[',-1},{'(',-2},{'{',-3},{']',1},{')',2},{'}',3}};
+    string isBalanced(string s){
+        stack<char> st;
+        for(char bracket: s){
+            if(symbols[bracket]<0){
+                st.push(bracket);
+            }else{
+                if(st.empty()) return "NO";
+                char top = st.top();
+                st.pop();
+                if(symbols[top] + symbols[bracket] != 0){
+                    return "NO";
+                }
+            }
+        }
+         if(st.empty()) return "YES";
+         return "NO";
+    }
+
 }
+
